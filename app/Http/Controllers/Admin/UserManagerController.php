bafcd69648c4f84532bc95fa7162d56a6fde3720
Le Cong Hau <anhlahau.hl@gmail.com>
2018-11-10 01:25:46 +0700

    Develop user namager function

---
+++ app/Http/Controllers/Admin/UserManagerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository;
+
+class UserManagerController extends Controller
+{
+    protected $user;
+
+    public function __construct(UserRepository $user)
+    {
+        $this->user = $user;
+    }
+
+    public function index(){
+        $users = $this->user->all();
+        return view('admin.users', compact('users'));
+    }
+
+    public function updateStatus(Request $request){
+        $status = $request->get('status');
+        $id=$request->get('id');
+        $this->user->updateStatus($status,$id);
+        return response()->json(['data'=>$status], 200);
+    }
+
+    public function detail($id){
+        $user = $this->user->getById($id);
+        return response()->json(['data'=>$user]);
+    }
+}
