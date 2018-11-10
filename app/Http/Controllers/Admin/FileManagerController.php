4be1254fecb7effd21cf5d827310bd46463f7cbd
Le Cong Hau <anhlahau.hl@gmail.com>
2018-11-10 10:55:23 +0700

    Develop manage files function

---
+++ app/Http/Controllers/Admin/FileManagerController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\FileRepository;
+
+class FileManagerController extends Controller
+{
+    protected $file;
+
+    public function __construct(FileRepository $file)
+    {
+        $this->file = $file;
+    }
+
+    public function index(){
+        $files = $this->file->all();
+        return view('admin.files', compact('files'));
+    }
+
+
+    public function delete($id){
+        $file = $this->file->getById($id);
+        $file->delete();
+        return response()->json(['data'=> 1 ], 200);
+    }
+
+    public function download($id)
+    {
+        $file = $this->file->getById($id);
+        $file_download= public_path(). $file->file_path;
+        $ext = pathinfo($file->file_path, PATHINFO_EXTENSION);
+        $headers = array(
+            'Content-Type: application/octet-stream',
+        );
+
+        return \Response::download($file_download, $file->file_name . '.'. $ext, $headers);
+    }
+}
