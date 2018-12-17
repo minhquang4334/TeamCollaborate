f5262d7494562b9b8f6888889370caf117400d26
thanh binh <binh97.lt@gmail.com>
2018-12-17 08:44:26 +0700

    fix front end reset pass

--- resources/js/components/user/auth/ForgotPassword.vue
+++ resources/js/components/user/auth/ForgotPassword.vue
@@ -1,39 +1,48 @@
 <template>
-    <div class="register-box">
-        <div class="register-logo">
-            <a>Team Collaborate</a>
-        </div>
 
-        <div class="register-box-body">
-            <p class="login-box-msg">Đăng ký tài khoản</p>
-
-            <form @submit.prevent="sendEmail" @keydown="clearErrors($event)">
-
-                <transition name="fade">
-                    <div v-if="hasAlert" :class="{ 'alert': true, 'alert-danger': !success, 'alert-success': success }">
-                        {{ message }}
-                    </div>
-                </transition>
-
-                <div :class="{ 'form-group': true, 'has-feedback': true, 'has-error': form.errors.has('email') }">
-                    <input v-model="form.email"
-                           type="text" name="email" title="Email"
-                           class="form-control" placeholder="Email">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    <has-error :form="form" field="email"></has-error>
+    <div class="login-homepage-form">
+        <div class="container">
+            <div class="content-login col-md-5 m-auto col-sm-8">
+                <div class="logo-left">
+                    <img src="/images/logo.jpg" class="size-img">
                 </div>
+                <div class="register-box">
+                    <div class="register-logo">
+                        <a>Team Collaborate</a>
+                    </div>
 
-                <div class="row">
-                    <div class="col-xs-8 col-xs-offset-2">
-                        <button :disabled="loading" type="submit" class="btn btn-primary btn-block btn-flat">
-                            <i v-if="loading" class="fa fa-spin fa-refresh"></i>
-                            Khôi phục mật khẩu
-                        </button>
+                    <div class="register-box-body mt-3">
+
+                        <form @submit.prevent="sendEmail" @keydown="clearErrors($event)">
+
+                            <transition name="fade">
+                                <div v-if="hasAlert" :class="{ 'alert': true, 'alert-danger': !success, 'alert-success': success }">
+                                    {{ message }}
+                                </div>
+                            </transition>
+
+                            <div :class="{ 'form-group': true, 'has-feedback': true, 'has-error': form.errors.has('email') }">
+                                <input v-model="form.email"
+                                       type="text" name="email" title="Email"
+                                       class="form-control" placeholder="Email">
+                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-xs-8 col-xs-offset-2 ml-3">
+                                    <button :disabled="loading" type="submit" class="btn btn-primary btn-block btn-flat">
+                                        <i v-if="loading" class="fa fa-spin fa-refresh"></i>
+                                        Khôi phục mật khẩu
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="margin-top-50 text-center">
+                            <router-link :to="{ name: 'login' }">Quay lại trang đăng nhập</router-link>
+                        </div>
                     </div>
                 </div>
-            </form>
-            <div class="margin-top-50 text-center">
-                <router-link :to="{ name: 'login' }">Quay lại trang đăng nhập</router-link>
             </div>
         </div>
     </div>
